cc1609b017dd3cacee68335c01638c71d7bb6a8a
Abdul-Momin-Rahman <a.mominatick@gmail.com>
2026-04-14 03:06:35 +0600

    Footer has been added and made responsive

--- src/UI/MyNavLink.jsx
+++ src/UI/MyNavLink.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router";
 
 const MyNavLink = ({to ,children, onClick}) => {
     return (
-        <NavLink to={to} onClick={onClick}  className={({ isActive }) => `btn btn-ghost text-[#64748B] font-medium px-4 py-3 rounded-lg ${isActive && ' bg-[#244D3F] text-white font-bold'}`}>
+        <NavLink to={to} onClick={onClick}  className={({ isActive }) => `btn btn-ghost text-[#64748B] font-medium text-base px-4 py-3 rounded-lg ${isActive && ' bg-[#244D3F] text-white font-bold'}`}>
             {children}
         </NavLink>
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import footerImg from '../../assets/logo-xl.png'
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
+import twitter from '../../assets/twitter.png'
 
 const Footer = () => {
     return (
-        <div>
-            I am Footer
-        </div>
+        <footer className='bg-[#244D3F] pt-20 pb-7.5 flex flex-col gap-10'>
+            <div className='flex flex-col gap-6'>
+                <div className='flex flex-col items-center gap-4 w-fit mx-auto'>
+                    <img src={footerImg} alt="footerImg" className='w-80 sm:w-105' />
+                    <p className='text-white/80  font-normal text-base text-center mx-15'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+                </div>
+
+                <div className='flex flex-col w-fit text-center gap-4 mx-auto'>
+                    <p className='font-medium text-xl text-white'>Social Links</p>
+
+                    <div className='flex gap-3'>
+                        <img src={instagram} alt="instagram" className='hover:scale-125 cursor-pointer ' />
+                        <img src={facebook} alt="facebook"  className='hover:scale-125 cursor-pointer '/>
+                        <img src={twitter} alt="twitter" className='hover:scale-125 cursor-pointer ' />
+                    </div>
+                </div>
+            </div>
+
+            <div className='border-t border-[#1A8862]/20 flex flex-col-reverse gap-5 sm:gap-15 sm:flex-row sm:w-9/12 justify-between mx-auto pt-7.5'>
+                <div className='text-base font-normal text-[#FAFAFA]/50 text-center'>© 2026 KeenKeeper. All rights reserved.</div>
+                <div className='flex justify-between gap-5 lg:gap-15'>
+                    <p className='text-base font-normal text-[#FAFAFA]/50 hover:text-white hover:underline transition duration-200 cursor-pointer'>Privacy Policy</p>
+                    <p className='text-base font-normal text-[#FAFAFA]/50 hover:text-white hover:underline transition duration-200 cursor-pointer'>Terms of Service</p>
+                    <p className='text-base font-normal text-[#FAFAFA]/50 hover:text-white hover:underline transition duration-200 cursor-pointer'>Cookies</p>
+                </div>
+            </div>
+        </footer>
     );
 };
 
